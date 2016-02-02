65179dbad91a2686fc0068129c38b6963410a56b
Rauljfa <rauljfa1@gmail.com>
2016-02-01 19:56:39 -0500

    Ventas

---
+++ ventas.js
@@ -0,0 +1,8 @@
+var ve1 = prompt ("Digite lo que gano en su primera venta");
+var ve2 = prompt ("Digite lo que gano en su segunda venta");
+var ve3 = prompt ("Digite lo que gano en su tercera venta");
+var sb = prompt ("Digite su salario base");
+var tvent = (ve1*0.1) + (ve2*0.1) + (ve3*0.1);
+var tsb = sb + tvent;
+alert("La comision total es " + tvent);
+alert ("Su saldo completo es " + tsb);
