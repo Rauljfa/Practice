06c97e85ec6c4b2630cf7f85577ece97a8181f13
Rauljfa <rauljfa1@gmail.com>
2016-02-09 23:27:23 -0500

    Update ventas.js

--- ventas.js
+++ ventas.js
@@ -3,6 +3,6 @@ var ve2 = prompt ("Digite lo que gano en su segunda venta");
 var ve3 = prompt ("Digite lo que gano en su tercera venta");
 var sb = prompt ("Digite su salario base");
 var tvent = (ve1*0.1) + (ve2*0.1) + (ve3*0.1);
-var tsb = sb + tvent;
+var tsb = parseInt (sb, 10) + tvent;
 alert("La comision total es " + tvent);
 alert ("Su saldo completo es " + tsb);
